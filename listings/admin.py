199b1b853c7c20125979eb1677686fcafed01757
saurabh patyal <saurabh.patyal@gmail.com>
2021-06-20 13:35:01 +0530

    Till admin dashboard customization

--- listings/admin.py
+++ listings/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
+from django.contrib.admin.filters import ListFilter
 from .models import Listing
 # Register your models here.
-admin.site.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display=('id' , 'title','is_published','price','list_date','realtor')
+    list_display_links=('id','title')
+    list_editable=('is_published',)
+    list_filter=('title','list_date','city')
+    list_per_page=20
+    search_fields=('city','title','realtor')
+
+
+
+
+admin.site.register(Listing,ListingAdmin)
--- realtors/admin.py
+++ realtors/admin.py
@@ -2,4 +2,14 @@ from realtors.models import Realtor
 from django.contrib import admin
 from .models import Realtor
 # Register your models here.
-admin.site.register(Realtor)
+class RealtorAdmin(admin.ModelAdmin):
+    list_display=('id','name','email','is_mvp','hire_date')
+    list_display_links=('id','name')
+    list_editable=('is_mvp',)
+    list_filter=('name','hire_date','email')
+    list_per_page=20
+    search_fields=('email','name')
+
+
+
+admin.site.register(Realtor,RealtorAdmin)
