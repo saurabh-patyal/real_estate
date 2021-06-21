8ea580e44ec2ebfa4ac929aaf08a5d01a0c7ddd4
saurabh patyal <saurabh.patyal@gmail.com>
2021-06-21 17:06:45 +0530

    Till section 7

--- listings/views.py
+++ listings/views.py
@@ -1,11 +1,26 @@
+from django.core import paginator
 from django.shortcuts import render
-
+from .models import Listing
+from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
 # Create your views here.
 def listings(request):
-    return render(request,'pages/listings.html')
+    #query to select recent listing which are published
+    listings=Listing.objects.order_by('-list_date').filter(is_published=True)
+
+#For showing pagination in listing
+
+    paginator=Paginator(listings,3)
+    page=request.GET.get('page')
+    paged_listings=paginator.get_page(page)
+
+    context={
+    
+        'listings': paged_listings
+    }
+    return render(request,'pages/listings.html', context)
 
 
-def listing(request):
+def listing(request,listing_id):
     return render(request,'pages/listing.html')
 
 
